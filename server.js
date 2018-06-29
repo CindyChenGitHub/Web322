b53b0c629ce54f27744de2484a3fe9c4a4403786
Cindy <blueapple.phoebe@gmail.com>
2018-06-29 14:30:43 -0400

    v1.0

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: _______Yue Chen_________ Student ID: ___150612166___ Date: ____June 15,2018____
+*  Name: _______YC_________ Student ID: ___*****___ Date: ____June 15,2018____
 *
 *  Online (Heroku) Link: _____https://young-coast-16371.herokuapp.com/ __________
 *
